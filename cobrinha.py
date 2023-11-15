e24854ae0f5f62f0cdbc48732af4a8e4c83f3e75
Alisson David de Melo <daviddust4@hotmail.com>
2023-11-15 14:23:37 -0300

    Update cobrinha.py
    Adicionando comentários ao código, para torna-lo mais explicativo.

--- cobrinha.py
+++ cobrinha.py
@@ -1,37 +1,19 @@
-import pygame
-import random
-
-pygame.init()
-
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
-GRID_SIZE = 20
-GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
-GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
-
-INITIAL_LENGTH = 3
-SPEED = 15
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Jogo da Cobrinha")
-clock = pygame.time.Clock()
-
+# Função para desenhar a cobra na tela
 def draw_snake(snake):
     for segment in snake:
         pygame.draw.rect(screen, GREEN, pygame.Rect(segment[0], segment[1], GRID_SIZE, GRID_SIZE))
 
+# Função para desenhar a comida na tela
 def draw_food(food_position):
     pygame.draw.rect(screen, RED, pygame.Rect(food_position[0], food_position[1], GRID_SIZE, GRID_SIZE))
 
+# Função para exibir a pontuação na tela
 def show_score(score):
     font = pygame.font.SysFont(None, 25)
     text = font.render("Score: " + str(score), True, GREEN)
     screen.blit(text, (10, 10))
 
+# Função para exibir a tela de "Game Over"
 def game_over():
     font = pygame.font.SysFont(None, 50)
     text = font.render("Perdeu kkk", True, GREEN)
@@ -40,6 +22,7 @@ def game_over():
     pygame.time.wait(2000)
     main()
 
+# Função principal que controla o jogo
 def main():
     snake = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
     snake_direction = (GRID_SIZE, 0)
@@ -53,6 +36,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        # Controle da direção da cobra com as setas do teclado
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and snake_direction != (GRID_SIZE, 0):
             snake_direction = (-GRID_SIZE, 0)
@@ -63,20 +47,25 @@ def main():
         elif keys[pygame.K_DOWN] and snake_direction != (0, -GRID_SIZE):
             snake_direction = (0, GRID_SIZE)
 
+        # Atualiza a posição da cabeça da cobra
         snake_head = ((snake[0][0] + snake_direction[0]), (snake[0][1] + snake_direction[1]))
         snake.insert(0, snake_head)
 
+        # Verifica se a cobra colidiu com a comida
         if snake_head == food_position:
             food_position = (random.randint(0, GRID_WIDTH - 1) * GRID_SIZE, random.randint(0, GRID_HEIGHT - 1) * GRID_SIZE)
             snake_length += 1
             score += 10
 
+        # Mantém o comprimento da cobra
         if len(snake) > snake_length:
             snake.pop()
 
+        # Verifica se a cobra colidiu com as bordas da tela
         if snake_head[0] < 0 or snake_head[0] >= SCREEN_WIDTH or snake_head[1] < 0 or snake_head[1] >= SCREEN_HEIGHT:
             game_over()
 
+        # Atualiza a tela do jogo
         screen.fill(BLACK)
         draw_snake(snake)
         draw_food(food_position)
@@ -87,5 +76,6 @@ def main():
 
     pygame.quit()
 
+# Inicia o jogo quando o script é executado
 if __name__ == "__main__":
     main()
